5a0f70e02a9df8e1ccce7ebcf3b79d3e793e8419
DESKTOP-U7P00EV\luket <lukeerlewein@gmail.com>
2025-06-24 16:07:01 -0400

    Assignment 1

---
+++ Homework/Week 1/Assignmentone.py
@@ -0,0 +1,20 @@
+#this function finds the correct place to put the "num" number and then inserts it into the correct position
+def insertionSort(inputList, num):
+    i = len(inputList) - 1
+    inputList.append(num)
+    while i >= 0 and inputList[i] > num:
+        inputList[i + 1] = inputList[i]
+        i -= 1
+    inputList[i + 1] = num
+
+
+def main():
+    sortedList = []
+    for i in range(8):
+        num = int(input(f"Enter next integer: "))
+        insertionSort(sortedList, num)
+        print("Current sorted array:", sortedList)
+
+
+if __name__ == "__main__":
+    main()
