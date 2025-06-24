89a7e3bb7baef36b9c339b72fa086ba3d39fc547
DESKTOP-U7P00EV\luket <lukeerlewein@gmail.com>
2025-06-24 16:11:34 -0400

    Title block update and output of program.

--- Homework/Week 1/Assignmentone.py
+++ Homework/Week 1/Assignmentone.py
@@ -1,3 +1,8 @@
+# Assignment 1: Insertion Sort
+# Student: Luke Erlewein
+# Professor: Dr. Denton Bobeldyk
+# Class: CIS 263 Summer 2025
+
 #this function finds the correct place to put the "num" number and then inserts it into the correct position
 def insertionSort(inputList, num):
     i = len(inputList) - 1
