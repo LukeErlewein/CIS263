99140a890be3d5324298899c039f8c4ab9685555
DESKTOP-U7P00EV\luket <lukeerlewein@gmail.com>
2025-07-01 19:03:46 -0400

    Assignment 3

---
+++ Homework/Week 2/Assignmentthree.py
@@ -0,0 +1,122 @@
+# Assignment 3: Binary Search Trees
+# Student: Luke Erlewein
+# Professor: Dr. Denton Bobeldyk
+# Class: CIS 263 Summer 2025
+
+# Part 1
+class TreeNode:
+    def __init__(self, value, parent=None):
+        self.value = value
+        self.parent = parent
+        self.left = None
+        self.right = None
+
+# I refer to the roots as root = TreeNode in the BST set up functions in part 4.
+
+# Part 2
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.value, end=" ")
+        inorder(node.right)
+
+def preorder(node):
+    if node:
+        print(node.value, end=" ")
+        preorder(node.left)
+        preorder(node.right)
+
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.value, end=" ")
+
+# Part 3
+def isBST(node, min = float('-inf'), max = float('inf')):
+    if node is None:
+        return True
+    if node.value <= min or node.value >= max:
+        return False
+    return isBST(node.left, min, node.value) and isBST(node.right, node.value, max)
+
+
+# Part 4
+# Find minimum node (used for successor)
+def findMinNode(node):
+    while node.left:
+        node = node.left
+    return node
+
+# Find a node by value
+def findNode(root, val):
+    if root is None or root.value == val:
+        return root
+    elif val < root.value:
+        return findNode(root.left, val)
+    else:
+        return findNode(root.right, val)
+
+# Find successor
+def findSuccessor(root, val):
+    node = findNode(root, val)
+    if node is None:
+        return None
+    if node.right:
+        return findMinNode(node.right)
+    successor = None
+    parent = root
+    while parent != node:
+        if node.value < parent.value:
+            successor = parent
+            parent = parent.left
+        else:
+            parent = parent.right
+    return successor
+
+
+# setups for BSTs
+def validBSTsetup():
+    nodes = {val: TreeNode(val) for val in [20, 10, 30, 5, 15, 25, 35, 13]}
+    root = nodes[20]
+    root.left = nodes[10]; nodes[10].parent = root
+    root.right = nodes[30]; nodes[30].parent = root
+    nodes[10].left = nodes[5]; nodes[5].parent = nodes[10]
+    nodes[10].right = nodes[15]; nodes[15].parent = nodes[10]
+    nodes[15].left = nodes[13]; nodes[13].parent = nodes[15]
+    nodes[30].left = nodes[25]; nodes[25].parent = nodes[30]
+    nodes[30].right = nodes[35]; nodes[35].parent = nodes[30]
+    return root
+
+def invalidBSTSetup():
+    nodes = {val: TreeNode(val) for val in [20, 10, 30, 5, 15, 25, 35, 12]}
+    root = nodes[20]
+    root.left = nodes[10]; nodes[10].parent = root
+    root.right = nodes[30]; nodes[30].parent = root
+    nodes[10].left = nodes[5]; nodes[5].parent = nodes[10]
+    nodes[10].right = nodes[15]; nodes[15].parent = nodes[10]
+    nodes[15].left = nodes[12]; nodes[12].parent = nodes[15]
+    nodes[15].right = nodes[25]; nodes[25].parent = nodes[15]
+    nodes[30].right = nodes[35]; nodes[35].parent = nodes[30]
+    return root
+
+# Print tree information
+def testTree(name, root, targetNode):
+    print(f"{name}:\n")
+    print("Inorder:   ", end=""); inorder(root); print()
+    print("Preorder:  ", end=""); preorder(root); print()
+    print("Postorder: ", end=""); postorder(root); print()
+    print("Is BST?:   ", isBST(root))
+    successor = findSuccessor(root, targetNode)
+    print(f"Successor of {targetNode} is {successor.value if successor else 'None'}\n\n\n")
+
+# Main function to run everything
+def main():
+    validBST = validBSTsetup()
+    invalidBST = invalidBSTSetup()
+    
+    testTree("Valid BST", validBST, 15)
+    testTree("Invalid BST", invalidBST, 15)
+
+if __name__ == "__main__":
+    main()
