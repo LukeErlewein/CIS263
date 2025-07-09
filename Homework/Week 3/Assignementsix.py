a25061373012ce51b6a2d2bff099b720d1cc10ea
DESKTOP-U7P00EV\luket <lukeerlewein@gmail.com>
2025-07-09 16:24:58 -0400

    Assignment 6 code

---
+++ Homework/Week 3/Assignementsix.py
@@ -0,0 +1,92 @@
+# Assignment 6: Hash Functions
+# Student: Luke Erlewein
+# Professor: Dr. Denton Bobeldyk
+# Class: CIS 263 Summer 2025
+
+import random
+
+TableSize = 10001
+ListSize = 500
+HashMod = 300
+
+# Second hash function for double hashing
+def h2(x):
+    return 9973 - (x % 9973)
+
+
+random.seed(616)
+InsertList = [random.randint(1, 100000) for _ in range(ListSize)]
+
+def linearProbing():
+    hashTable = [None] * TableSize
+    numCollide = 0
+
+    for currentVar in InsertList:
+        index = currentVar % HashMod
+
+        while hashTable[index] is not None:
+            numCollide += 1
+            index = (index + 1) % TableSize
+        hashTable[index] = currentVar
+
+    return numCollide
+
+def quadraticProbing(c1=1, c2=2):
+    hashTable = [None] * TableSize
+    numCollide = 0
+
+    for currentVar in InsertList:
+        index = currentVar % HashMod
+        i = 0
+        while 1:
+            newIndex = (index + c1 * i + c2 * i^2) % TableSize
+            if hashTable[newIndex] is None:
+                hashTable[newIndex] = currentVar
+                break
+            numCollide += 1
+            i += 1
+
+    return numCollide
+
+def doubleHash():
+    hashTable = [None] * TableSize
+    numCollide = 0
+
+    for currentVar in InsertList:
+        index1 = currentVar % HashMod
+        step = h2(currentVar)
+        i = 0
+        while True:
+            newIndex = (index1 + (i * step)) % TableSize
+            if hashTable[newIndex] is None:
+                hashTable[newIndex] = currentVar
+                break
+            numCollide += 1
+            i += 1
+
+    return numCollide
+
+def modifiedLinearProbing():
+    hashTable = [None] * TableSize
+    numCollide = 0
+
+    for currentVar in InsertList:
+        index = currentVar % HashMod
+
+        while hashTable[index] is not None:
+            numCollide += 1
+            index = (index + 10) % TableSize
+        hashTable[index] = currentVar
+
+    return numCollide
+
+# Run all strategies
+linear = linearProbing()
+quadratic = quadraticProbing()
+double = doubleHash()
+modifiedLinear = modifiedLinearProbing()
+
+print(f"numCollide (Linear Probing): {linear}")
+print(f"numCollide (Quadratic Probing): {quadratic}")
+print(f"numCollide (Double Hash): {double}")
+print(f"numCollide (Modified Linear Probing): {modifiedLinear}")
